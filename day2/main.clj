232578c5b216aca58b94b2d2908ba6d6e0f51fff
Zachary Maillard <zmaillard@healthwise.org>
2024-12-03 19:57:11 -0700

    Day 2

--- day2/main.clj
+++ day2/main.clj
@@ -3,7 +3,9 @@
            [clojure.java.io :as io]
            [clojure.string :as str])) 
 
-
+(defn remove-item-by-index
+ [items i]
+ (concat (subvec items 0 i) (subvec items (inc i))))
 
 (defn get-items
   [acc line]
@@ -22,12 +24,13 @@
         within-range (every? #(and(< (abs %) 4)(> (abs %) 0))delta)]
     (and(or all-neg all-pos) within-range)))
 
-(defn check-one-unsafe
-  [delta]
-  (let [all-neg (every? #(< % 0) delta)
-        all-pos (every? #(> % 0) delta)
-        within-range (every? #(and(< (abs %) 4)(> (abs %) 0))delta)]
-    (and(or all-neg all-pos) within-range)))
+
+;(defn check-one-unsafe
+;  [delta]
+;  (let [neg (map #(< % 0) delta)
+;        pos (map #(> % 0) delta)
+;        within-range (map #(and(< (abs %) 4)(> (abs %) 0))delta))
+;    (mapv #(or %1 %2 %3) neg pos within-range)))
     
 (defn get-deltas
   [line]
@@ -35,7 +38,21 @@
       (partition 2 1)
       (map #(- (nth % 0)(nth % 1)))
       (check)))
-      
+
+(defn do-unsafe-check
+  [orig-line delta]
+  (let [all-neg (every? #(< % 0) delta)
+        all-pos (every? #(> % 0) delta)
+        within-range (every? #(and(< (abs %) 4)(> (abs %) 0))delta)
+        is-good (and(or all-neg all-pos) within-range)]
+    (if is-good is-good (reduce (fn[acc l] (or acc (get-deltas l))   ) false (map #(remove-item-by-index orig-line %)  (range(count orig-line)))))))
+
+(defn get-deltas-unsafe
+  [line]
+  (->> line
+      (partition 2 1)
+      (map #(- (nth % 0)(nth % 1)))
+      (do-unsafe-check line)))
 
 ; part 1 - All Unsafe
 (defn all-unsafe
@@ -43,3 +60,9 @@
   (println(count (filter true? (map get-deltas (read-file))))))
 
 
+; part 2
+(defn one-unsafe
+  []
+  (println(count (filter true? (map get-deltas-unsafe (read-file))))))
+
+(one-unsafe)
